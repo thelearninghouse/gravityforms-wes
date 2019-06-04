7b60ba57bf3209414591fb3396f7052852bed34c
Kurt Rank <krank@learninghouse.com>
2019-06-04 12:11:01 -0400

    fix: Update wp_register_script to use min version

--- gravityforms-wes.php
+++ gravityforms-wes.php
@@ -16,7 +16,7 @@ add_action( 'gform_field_content', 'tlh_gravity_forms_modify_fields', 10, 2 );
 
 function tlh_gravity_forms_populate() {
 	wp_register_script( 'js-cookie', 'https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js', false );
-	wp_register_script( 'tlh-gravity-forms-populate', plugins_url( '/public/js/tlh-gravity-forms-populate.js', __FILE__ ), false );
+	wp_register_script( 'tlh-gravity-forms-populate', plugins_url( '/public/js/tlh-gravity-forms-populate.min.js', __FILE__ ), false );
 
 	wp_enqueue_script( 'js-cookie' );
 	wp_enqueue_script( 'tlh-gravity-forms-populate' );
