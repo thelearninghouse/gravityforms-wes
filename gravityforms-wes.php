b1d0f972c4de74e84ca2847788447c8945c23f3c
Kurt Rank <krank@learninghouse.com>
2019-06-07 11:25:39 -0400

    fix: Add self-hosted minified version of js-cookie

--- gravityforms-wes.php
+++ gravityforms-wes.php
@@ -15,8 +15,8 @@ function tlh_gravity_forms_modify_fields( $field_content, $field ) {
 add_action( 'gform_field_content', 'tlh_gravity_forms_modify_fields', 10, 2 );
 
 function tlh_gravity_forms_populate() {
-	wp_register_script( 'js-cookie', 'https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js', false );
-	wp_register_script( 'tlh-gravity-forms-populate', plugins_url( '/public/js/tlh-gravity-forms-populate.min.js', __FILE__ ), false );
+	wp_register_script( 'js-cookie', plugins_url( '/public/js/js-cookie.min.js', __FILE__ ), false );
+	wp_register_script( 'tlh-gravity-forms-populate', plugins_url( '/public/js/tlh-gravity-forms-populate.min.js', __FILE__ ), array( 'js-cookie' ), '1.0', true );
 
 	wp_enqueue_script( 'js-cookie' );
 	wp_enqueue_script( 'tlh-gravity-forms-populate' );
@@ -26,7 +26,7 @@ add_action( 'wp_enqueue_scripts', 'tlh_gravity_forms_populate' );
 // add async attribute
 function tlh_gravity_forms_async_attribute( $tag, $handle ) {
 	// add script handles to the array below
-	$scripts_to_async = array( 'js-cookie', 'tlh-gravity-forms-populate' );
+	$scripts_to_async = array( 'tlh-gravity-forms-populate' );
 
 	foreach ( $scripts_to_async as $async_script ) {
 		if ( $async_script === $handle ) {
