b6551420114c2c5f3b2b261bce0ef8f81eec2051
Kurt Rank <krank@learninghouse.com>
2019-06-19 13:27:38 -0400

    fix: Update uadgroup and uadcampgn field names

--- gravityforms-wes.php
+++ gravityforms-wes.php
@@ -3,7 +3,7 @@
  * Plugin Name: Gravity Forms - WES
  * Description: Manage cookies and add them to Gravity Forms
  * Author: Kurt Rank
- * Version: 1.0.4
+ * Version: 1.0.5
  */
 
 function tlh_gravity_forms_modify_fields( $field_content, $field ) {
